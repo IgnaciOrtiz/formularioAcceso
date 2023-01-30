0a60ab2326b993a292e990e59de3ed317eacb220
Ignacio Ortiz <iortizalonzua@gmail.com>
2023-01-30 20:35:32 +0100

    Commit5

--- formularioAcceso/resources/views/auth/registro.blade.php
+++ formularioAcceso/resources/views/auth/registro.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/app.css" type="text/css" rel="stylesheet" >
     <title>Registro</title>
+    
 </head>
 <body>
     @if($errors->any())
@@ -22,16 +24,18 @@
             <p>{{ session('mensaje') }}</p>
         </div>
     @endif
-    
-    <form action="/registro" method="POST">
+    <section class="form-container">
+    <form class="form" action="/registro" method="POST">
+        <h4>Formulario Registro Laravel</h4>
         @csrf
-        <input type="text" name="nombre">
-        <input type="text" name="apellidos">
-        <input type="text" name="edad">
-        <input type="email" name="email">
-        <input type="password" name="password">
-        <input type="password" name="password_confirm">
-        <input type="submit" value="GuardarUsuario">
+        <input class="campos" type="text" name="nombre" placeholder="Nombre">
+        <input class="campos"  type="text" name="apellidos" placeholder="Apellidos">
+        <input class="campos"  type="text" name="edad" placeholder="Edad">
+        <input class="campos"  type="email" name="email" placeholder="Email">
+        <input class="campos"  type="password" name="password" placeholder="Constraseña">
+        <input class="campos"  type="password" name="password_confirm" placeholder="Repita Contraseña">
+        <input class=button type="submit" value="Guardar">
     </form>
+    </section>
 </body>
 </html>
